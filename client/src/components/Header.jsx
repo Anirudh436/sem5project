466e21bd28676c12ee505814dcc0499bea157437
Anirudh <anpreech.123@gmail.com>
2024-10-07 19:31:11 +0530

    sdfsjhsbf

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
   const {currentUser} = useSelector(state => state.user) 
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate=useNavigate();
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className='bg-blue-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -14,9 +34,16 @@ export default function Header() {
         <span className='text-blue-700'>Estate</span>
       </h1>
       </Link>
-      <form className='bg-blue-100 p-3 rounded-lg flex items-center'>
-        <input type='text' placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64 '></input>
-        <FaSearch className='text-blue-500'/>
+      <form  onSubmit={handleSubmit}
+      className='bg-blue-100 p-3 rounded-lg flex items-center'>
+        <input 
+        type='text'
+         placeholder="Search..."
+          className='bg-transparent focus:outline-none w-24 sm:w-64 '
+             onChange={(e) => setSearchTerm(e.target.value)}/>
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
       </form>
       <ul className='flex gap-3'>
         <Link to='/'>
@@ -29,7 +56,10 @@ export default function Header() {
   {
     currentUser ? (
       currentUser.avatar ? (
-        <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="Profile" />
+        <img 
+        className='rounded-full h-7 w-7 object-cover' 
+        src={currentUser.avatar} 
+        alt="Profile" />
       ) : (
         <p>Loading...</p> // Show a loader or placeholder while waiting for the image URL
       )
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,55 +1,58 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { useSelector } from 'react-redux';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
 import {
-    FaBath,
-    FaBed,
-    FaChair,
-    FaMapMarkedAlt,
-    FaMapMarkerAlt,
-    FaParking,
-    FaShare,
-  } from 'react-icons/fa';
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from 'react-icons/fa';
+import Contact from '../components/Contact';
+
+// https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
 export default function Listing() {
-    SwiperCore.use([Navigation]);
-    const [listing, setListing] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [copied, setCopied] = useState(false);
+  SwiperCore.use([Navigation]);
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
+  const params = useParams();
+  const { currentUser } = useSelector((state) => state.user);
 
-    const params = useParams();
-    useEffect(()=>{
-        const fetchListing = async () =>{
-            try {
-                setLoading(true);
-                const res = await fetch(`/api/listing/get/${params.listingId}`);
-                const data = await res.json();
-                if (data.success === false){
-                    setError(true);
-                    setLoading(false);
-                    return;
-                }
-                setListing(data);
-                setLoading(false);
-                
-            } catch (error) {
-                setError(true);
-                setLoading(false);
-            }
-        };
-        fetchListing();
-    },[params.listingId]);
-    console.log(loading);
-    
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
+
   return (
     <main>
-       {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+      {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
       {error && (
         <p className='text-center my-7 text-2xl'>Something went wrong!</p>
       )}
@@ -133,9 +136,18 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            </div>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
-         )}   
-         </main>
-)
+        </div>
+      )}
+    </main>
+  );
 }
